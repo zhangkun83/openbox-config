3087d179298f6ce2c21959a0f1c3932b9d61cbc3
Kun Zhang <zhangkun@google.com>
2018-06-07 06:46:53 -0700

    Add wallpaper.

---
+++ scripts/prepare-wallpaper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+script_path="$(readlink -f "$0")"
+script_dir="$(dirname "$script_path")"
+parentdir="$(dirname "$script_dir")"
+WALLPAPER_SRC_DIR="$parentdir/runtime/wallpapers"
+wallpaper=$(ls -1 "$WALLPAPER_SRC_DIR" | shuf | head -1)
+if [ -z "$wallpaper" ]; then
+    wallpaper="wallpaper.jpg"
+    wallpaper_path="$script_dir/$wallpaper"
+else
+    wallpaper_path="$WALLPAPER_SRC_DIR/$wallpaper"
+fi
+# If the file name ends with "-tile", it's a tile image
+if [[ "$wallpaper" == *-tile.* ]]; then
+    option="--bg-tile"
+else
+    option="--bg-fill"
+fi
+feh "$option" "$wallpaper_path"
