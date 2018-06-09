6f93b6a583358410f90bd53fd3abe3e979f7c349
Kun Zhang <zhangkun@google.com>
2018-06-08 17:30:52 -0700

    Use start_if_absent.sh to start wallpaper-auto-switcher
    Let start_if_absent.sh return non-zero if the target is already
    started, in which case autostart will run prepare-wallpaper to set
    the wallpaper.
    
    There was a bug in the previous comment that I didn't run
    wallpaper-auto-switcher.sh with '&', which blocks the autostart
    script.  This will go away in this change.

--- scripts/start_if_absent.sh
+++ scripts/start_if_absent.sh
@@ -5,8 +5,10 @@ shift
 RESULT=$(ps -u $USER -o command | grep -F "$EXEC" | grep -v '\(^grep \)\|\(start_if_absent\.sh \)')
 
 if [ -z "$RESULT" ]; then
-  $@ &
-  echo "start_if_absent: $EXEC started"
+    $@ &
+    echo "start_if_absent: $EXEC started"
+    return 0
 else
-  echo "start_if_absent: $EXEC already running"
+    echo "start_if_absent: $EXEC already running"
+    return 1
 fi
